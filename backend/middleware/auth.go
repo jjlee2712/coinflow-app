5005fa177caba74a76a730f78ae9e21d66492e45
JJLee <junjielee15@gmail.com>
2026-09-24 21:45:23 +0800

    Implement middleware for validate token

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -1,1 +1,34 @@
 package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/jjlee2712/coinflow/backend/service"
+)
+
+type contextKey string
+
+const UserIDKey contextKey = "userID"
+
+func AuthMiddleware(jwtSecret string) func(http.Handler)  http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer "){
+        http.Error(w, "missing or invalid token", http.StatusUnauthorized)
+        return
+			}
+      tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+      userID, err := service.ValidateToken(tokenStr, jwtSecret)
+      if err != nil {
+        http.Error(w, "invalid token", http.StatusUnauthorized)
+        return
+      }
+      ctx := context.WithValue(r.Context(), UserIDKey, userID)
+      next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
